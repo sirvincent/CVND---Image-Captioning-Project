41920d5f619f07a9a10cc2900859d528520db956
Vincent Post <v.post@condifood.com>
2020-04-26 18:24:30 +0200

    ADD: files for submission

--- model.py
+++ model.py
@@ -23,11 +23,52 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
+        super(DecoderRNN, self).__init__()
+        
+        self.hidden_size = hidden_size
+        
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+        
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers=num_layers, batch_first=True)
+        
+        self.linear = nn.Linear(hidden_size, vocab_size)
+
+        
     
     def forward(self, features, captions):
-        pass
+        embeddings = self.embedding(captions[:, :-1])
+        features = features.unsqueeze(1)
+        
+        embedded_input = torch.cat((features, embeddings), 1)
+        
+        # TODO: is the hidden state necessary? it is unused currently so no need to assign a variable for it
+        lstm_output, _ = self.lstm(embedded_input)
+        
+        output = self.linear(lstm_output)
+        
+        return output
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+        predicted_word_tokens = []
+        for idx in range(max_len):
+            predictions, states = self.lstm(inputs, states)
+            predictions = self.linear(predictions)
+            
+            predicted_word_token = predictions.argmax()
+            
+            # TODO: magic number 1, pas end token as parameter
+            if predicted_word_token == 1:
+                # end word has been found
+                break
+                
+            predicted_word_tokens.append(predicted_word_token.item())
+
+            # feed latest state into new state of lstm      
+            inputs = self.embedding(predicted_word_token)   
+            inputs = inputs.unsqueeze(0).unsqueeze(0)
+        
+        return predicted_word_tokens
+            
+            
+        
